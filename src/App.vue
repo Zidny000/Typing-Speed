f63b6f3d3bbc1d6b986707fe541176a87747acd4
zidny000 <t.m.zidny@gmail.com>
2022-02-12 23:51:48 +0600

    done

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <script setup>
 import Input from './components/Input.vue'
 import { ref,watchEffect,onUpdated } from 'vue'
-
 const api = 'http://api.quotable.io/random'
 const quote = ref("");
 let el = ref(null)
-
 
 function getRandomQuote() {
   return fetch(api)
     .then(response => response.json())
     .then(data => data.content)
 }
-
 const processQuote = async (ex)=>{
   quote.value = await getRandomQuote()
   
 }
-
 watchEffect(async () => {
   await processQuote()
   
   })
-
 onUpdated(() => {
  
   Array.from(el.value.children).forEach(char =>{
@@ -32,18 +27,15 @@ onUpdated(() => {
     })
   
 })
-
 function spans() {
   return Array.from(el.value.children)
 }
-
-
 </script>
 
 <template>
-<div class="timer" id="timer"></div>
-  <div class="container"  >
-  
+
+
+  <div class="container">
   <div class="quote-display" id="quoteDisplay" v-if="quote" ref="el">
    <span v-for="char in quote.split('')" >{{char}}</span>
     
--- src/components/Input.vue
+++ src/components/Input.vue
@@ -2,49 +2,83 @@
 import { ref , onBeforeMount,onUpdated} from 'vue'
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-
 let data = ref([])
 let input = ref(null)
+let totalChar = ref(0)
+let rightChar = ref(0)
+let totalCharA = ref(null)
+let rightCharA = ref(null)
+let time = ref(60);
+
+let timer;
+function reset(){
+  time.value = 60;
+  totalCharA.value = null;
+  rightCharA.value = null;
+  rightChar = 0;
+  totalChar = 0;
+  input.value = null
+  props.pro();
+}
+function start(){
+  
+  timer = setInterval(()=>{
+    time.value--
+    if(time.value == 0){
+      totalCharA.value = totalChar.value
+      if(input.value != null){
+        data.value.forEach((char,i)=>{     
+        if(char.innerText == input.value[i]){
+          rightChar.value++
+        }
+        })
+      }
+     
+      rightCharA.value = rightChar.value
+      clearInterval(timer)   
+    }
+  
+  },1000);
+}
 const props = defineProps({
   arrayQuote:Function,
   pro:Function
 })
-
 function execute() {
+  totalChar.value++
   let correct = true
-  props.arrayQuote().forEach((characterSpan,index)=>{
+  data.value = props.arrayQuote()
+  data.value.forEach((characterSpan,index)=>{
   if(input.value[index] == null){
     characterSpan.classList.remove('correct')
     characterSpan.classList.remove('incorrect')
     correct = false
-    console.log("z")
   }else if(characterSpan.innerText === input.value[index]){
     characterSpan.classList.add('correct')
     characterSpan.classList.remove('incorrect')
-    console.log("y")
   }else{
     characterSpan.classList.remove('correct')
     characterSpan.classList.add('incorrect')
     correct = false
-    console.log("z")
   }
-
   })
-console.log(correct)
-
-if(correct){
-  
-  input.value = null
-  props.pro();
-  
-} 
+  console.log(correct)
+  if(correct){
+    rightChar.value = rightChar.value + input.value.split('').length
+    input.value = null
+    props.pro();
+  } 
 }
-
 </script>
 
 <template>
   
-  <textarea id="quoteInput" class="quote-input" v-model="input" @input='execute' autofocus></textarea>
+  <textarea id="quoteInput" class="quote-input" v-model="input" @input='execute' autofocus  ></textarea>
+  <div class="point" id="timer"> Timer : {{time}}</div>
+  <button @click="start">Start</button>
+  <button @click="reset">Reset</button>
+  <div class="point" id="timer"> Total Entries : {{totalCharA}}</div>
+  <div class="point" id="timer"> Right Entries : {{rightCharA}}</div>
+  
   
 </template>
-
